690559c683f5c5353ff947614b7c6c5703edf972
James Atkinson <jamesmtb@gmail.com>
2020-10-06 16:11:03 +0100

    Added rules code

---
+++ keyboards/kyria/keymaps/jamesatkinson/rules.mk
@@ -0,0 +1,8 @@
+# Enable the use of OLED displays
+OLED_DRIVER_ENABLE = yes
+
+# Enable the use of one or more encoders
+ENCODER_ENABLE = yes
+
+# Enable keyboard RGB underglow
+RGBLIGHT_ENABLE = yes
