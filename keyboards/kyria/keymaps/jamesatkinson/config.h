ff91124882baddd6b723d451bb1e778a1ba27103
James Atkinson <jamesmtb@gmail.com>
2020-10-06 16:01:58 +0100

    Added config code

---
+++ keyboards/kyria/keymaps/jamesatkinson/config.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#ifdef OLED_DRIVER_ENABLE
+    #define OLED_DISPLAY_128X64
+#endif
+
+#ifdef RGBLIGHT_ENABLE
+    #define RGBLIGHT_ANIMATIONS
+    #define RGBLIGHT_HUE_STEP 8
+    #define RGBLIGHT_SAT_STEP 8
+    #define RGBLIGHT_VAL_STEP 8
+#endif
+
+#define SPLIT_USB_DETECT
+#define NO_USB_STARTUP_CHECK
